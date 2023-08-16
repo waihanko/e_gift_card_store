1588e86d3f9fb586bf3e4f0aa880eeae05bedf21
wai_han_ko <waihanko@tamronyangon.com>
2023-08-16 17:30:02 +0630

    - Scroll Tabs

--- lib/app/features/gift_card_item_list/gift_card_detail_screen.dart
+++ lib/app/features/gift_card_item_list/gift_card_detail_screen.dart
@@ -3,6 +3,8 @@ import '../../constants/resources/app_colors.dart';
 import '../../constants/resources/app_dimens.dart';
 import '../../widgets/rounded_icon_widget.dart';
 import '../../widgets/text_view_widget.dart';
+import 'package:collection/collection.dart';
+
 
 class GiftCardDetailScreen extends StatefulWidget {
   final String title;
@@ -10,14 +12,18 @@ class GiftCardDetailScreen extends StatefulWidget {
   const GiftCardDetailScreen({super.key, required this.title});
 
   @override
-  State<GiftCardDetailScreen> createState() => _GiftCardDetailScreenState();
+  State<GiftCardDetailScreen> createState() => _GiftCardDetailScreenStates();
 }
 
-class _GiftCardDetailScreenState extends State<GiftCardDetailScreen>
+class _GiftCardDetailScreenStates extends State<GiftCardDetailScreen>
     with TickerProviderStateMixin {
   late ScrollController _controller;
   bool sliverActionsHidden = false;
+  BuildContext? tabContext;
   double opacity = 0;
+  double toolBarHeight = 150;
+  double tempDynamicHeight = 200;
+  late List<double> itemStartPoint;
   final List<GlobalKey<State<StatefulWidget>>> dataKey = [
     GlobalKey(),
     GlobalKey(),
@@ -27,8 +33,31 @@ class _GiftCardDetailScreenState extends State<GiftCardDetailScreen>
 
   void scrollListener() {
     setState(() {
-      opacity =((_controller.position.pixels)/ (MediaQuery.sizeOf(context).height * 0.2) ).clamp(0.0, 1.0);
+      opacity = (_controller.position.pixels / toolBarHeight).clamp(0.0, 1.0);
     });
+
+    List<double> heightOfWidget = dataKey
+        .mapIndexed((index, item) =>
+            ((item.currentContext?.findRenderObject() as RenderBox)
+                .size
+                .height))
+        .toList();
+
+    double previousValue = 0.0;
+    itemStartPoint = [
+      0.0,
+      ...heightOfWidget.map((value) {
+        double newValue = previousValue + value;
+        previousValue = newValue;
+        return newValue + toolBarHeight;
+      })
+    ];
+
+    int? index = itemStartPoint
+        .lastIndexWhere((value) => _controller.position.pixels >= value);
+
+    DefaultTabController.of(tabContext!)
+        .animateTo(index<0?0:index, duration: const Duration(milliseconds: 300));
   }
 
   @override
@@ -40,144 +69,171 @@ class _GiftCardDetailScreenState extends State<GiftCardDetailScreen>
 
   @override
   Widget build(BuildContext context) {
-    TabController tabController = TabController(length: 4, vsync: this);
-    return Scaffold(
-      extendBody: true,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: AppColors.primaryColor.withOpacity(opacity),
-        surfaceTintColor: Colors.transparent,
-        bottomOpacity: opacity,
-        centerTitle: true,
-        iconTheme: IconThemeData(
-            color: opacity < 0.5 ? AppColors.primaryColor : Colors.black),
-        actions: [
-          RoundedIconWidget(
-            icon: Icon(
-              Icons.favorite_outline_rounded,
+    return DefaultTabController(
+      length: 4,
+      child: Builder(builder: (context) {
+        tabContext = context;
+        return Scaffold(
+          extendBody: false,
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: AppColors.primaryColor.withOpacity(opacity),
+            surfaceTintColor: Colors.transparent,
+            bottomOpacity: opacity,
+            centerTitle: true,
+            iconTheme: IconThemeData(
+                color: opacity < 0.5 ? AppColors.primaryColor : Colors.black),
+            actions: [
+              RoundedIconWidget(
+                icon: Icon(
+                  Icons.favorite_outline_rounded,
+                ),
+                backgroundColor: Colors.transparent,
+              )
+            ],
+            title: TextViewWidget(
+              "Scroll Tabs",
+              textColor: AppColors.primaryTextColor.withOpacity(opacity),
             ),
-            backgroundColor: Colors.transparent,
-          )
-        ],
-        title: TextViewWidget(
-          "Mobile Legend Diamond",
-          textColor: AppColors.primaryTextColor.withOpacity(opacity),
-        ),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(28),
-          child: TabBar(
-              indicatorColor: AppColors.secondaryButtonColor,
-              controller: tabController,
-              unselectedLabelColor: AppColors.secondaryTextColor,
-              dividerColor: Colors.transparent,
-              onTap: (index) => {
-                tabController.index = index,
-                print(tabController.index),
-                    Scrollable.ensureVisible(dataKey[index].currentContext!,
-                        duration: const Duration(milliseconds: 600))
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(28),
+              child: TabBar(
+                  indicatorColor: AppColors.secondaryButtonColor,
+                  unselectedLabelColor: AppColors.secondaryTextColor,
+                  dividerColor: Colors.transparent,
+                  onTap: (index) {
+                    _controller.position.animateTo(
+                      itemStartPoint[index],
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.linear,
+                    );
                   },
-              tabs: const [
-                Tab(
-                  height: 28,
-                  text: "Part 1",
-                ),
-                Tab(
-                  height: 28,
-                  text: "Part 2",
-                ),
-                Tab(
-                  height: 28,
-                  text: "Part 3",
-                ),
-                Tab(height: 28, text: "Part 4"),
-              ]),
-        ),
-      ),
-      body: CustomScrollView(
-        controller: _controller,
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            key: dataKey[0],
-            toolbarHeight: MediaQuery.sizeOf(context).height * 0.2,
-            automaticallyImplyLeading: false,
-            stretch: true,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage(
-                              "assets/images/test.png",
-                            ),
-                            fit: BoxFit.cover)),
-                    padding: EdgeInsets.all(AppDimens.marginCardMedium2),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            height:
-                                kToolbarHeight + AppDimens.marginCardMedium),
-                        Row(
+                  tabs: const [
+                    Tab(
+                      height: 28,
+                      text: "Part 1",
+                    ),
+                    Tab(
+                      height: 28,
+                      text: "Part 2",
+                    ),
+                    Tab(
+                      height: 28,
+                      text: "Part 3",
+                    ),
+                    Tab(
+                      height: 28,
+                      text: "Part 4",
+                    ),
+                  ]),
+            ),
+          ),
+          body: CustomScrollView(
+            controller: _controller,
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              SliverAppBar(
+                toolbarHeight: toolBarHeight,
+                automaticallyImplyLeading: false,
+                stretch: true,
+                flexibleSpace: FlexibleSpaceBar(
+
+                  background: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                  "assets/images/test.png",
+                                ),
+                                fit: BoxFit.cover)),
+                        // color: AppColors.primaryColor,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(AppDimens.marginCardMedium2),
+                        child: Column(
                           children: [
-                            Container(
-                              width: 86,
-                              height: 120,
-                              color: AppColors.secondaryButtonColor,
+                            SizedBox(
+                                height: kToolbarHeight +
+                                    AppDimens.marginCardMedium),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 86,
+                                  height: 120,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                            "assets/images/img.png",
+                                          ),
+                                          fit: BoxFit.cover)),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                    // color: AppColors.primaryColor,
-                  ),
-                  Positioned(
-                    bottom: -2,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      height: 14,
-                      decoration: const BoxDecoration(
-                        color: AppColors.primaryColor,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(AppDimens.marginXLarge),
-                          topRight: Radius.circular(AppDimens.marginXLarge),
+                      ),
+                      Positioned(
+                        bottom: -2,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 14,
+                          decoration: const BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(AppDimens.marginXLarge),
+                              topRight: Radius.circular(AppDimens.marginXLarge),
+                            ),
+                          ),
                         ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          tempDynamicHeight = tempDynamicHeight + 40;
+                        });
+                      },
+                      child: Container(
+                        key: dataKey[0],
+                        height: tempDynamicHeight,
+                        color: Colors.white,
+                        child: Center(child: TextViewWidget("Part 1", textSize: AppDimens.textHeading2X,)),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
+                    Container(
+                      key: dataKey[1],
+                      height: 120,
+                      color: Color(0xFF6F85B7),
+                      child: Center(child: TextViewWidget("Part 2" , textSize: AppDimens.textHeading2X,)),
+                    ),
+                    Container(
+                      key: dataKey[2],
+                      height: 600,
+                      color:  Color(0xFF94B498),
+                      child: Center(child: TextViewWidget("Part 3",textSize: AppDimens.textHeading2X,)),
+                    ),
+                    Container(
+                      key: dataKey[3],
+                      height: 800,
+                      color: Color(0xFFA2B5BB),
+                      child: Center(child: TextViewWidget("Part 4",textSize: AppDimens.textHeading2X,)),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          SliverList(
-              delegate: SliverChildListDelegate([
-            Container(
-              height: 500,
-              color: Colors.yellow,
-              child: Center(child: Text("Section 1")),
-            ),
-            Container(
-              key: dataKey[1],
-              height: 500,
-              color: Colors.red,
-              child: Center(child: Text("Section 2")),
-            ),
-            Container(
-              key: dataKey[2],
-              height: 500,
-              color: Colors.green,
-              child: Center(child: Text("Section 3")),
-            ),
-            Container(
-              key: dataKey[3],
-              height: 500,
-              color: Colors.grey,
-              child: Center(child: Text("Section 4")),
-            ),
-          ]))
-        ],
-      ),
+        );
+      }),
     );
   }
 }
